9c04402c56c909530e496179865e62f23dd87940
Onedrican <v.bernardini@hispeed.ch>
2023-12-11 15:42:59 +0100

    form mit aktualiesire gmacht

--- Angebot.php
+++ Angebot.php
@@ -8,79 +8,131 @@
 </head>
 <body>
 <?php
+
+$SmallServer = [
+  "Cores" => 4,
+  "Ram"   => 32768,
+  "SSD"   => 4000,
+];
+
+$MediumServer = [
+  "Cores" => 8,
+  "Ram"   => 65536,
+  "SSD"   => 8000,
+];
+
+$BigServer = [
+  "Cores" => 16,
+  "Ram"   => 131072,
+  "SSD"   => 16000,
+];
+
+$ArraySmall = implode(";",$SmallServer);
+$ArrayMedium = implode(";",$MediumServer);
+$ArrayBig = implode(";",$BigServer);
+$file="serverleistung.txt";
+$AllgemeinServerleistung = "Small".";". $ArraySmall . "\n" ."Medium". ";" . $ArrayMedium . "\n" . "Big". ";" . $ArrayBig . "\n";
+file_put_contents($file,$AllgemeinServerleistung);
+
         if ($_SERVER["REQUEST_METHOD"] == "POST") {
             $selectedCpu = intval($_POST['cpu']);
             $selectedRam = intval($_POST['ram']);
             $selectedSsd = intval($_POST['ssd']);
+
+            
+            $smallServerCpu = $SmallServer["Cores"];
+            $smallServerRam = $SmallServer["Ram"];
+            $smallServerSsd = $SmallServer["SSD"];
+            
+            $mediumServerCpu = $MediumServer["Cores"];
+            $mediumServerRam = $MediumServer["Ram"];
+            $mediumServerSsd = $MediumServer["SSD"];
+            
+            $bigServerCpu = $BigServer["Cores"];
+            $bigServerRam = $BigServer["Ram"];
+            $bigServerSsd = $BigServer["SSD"];
+            
+            switch (true) {
+                case ($selectedCpu <= $smallServerCpu && $selectedRam <= $smallServerRam && $selectedSsd <= $smallServerSsd):
+                    echo "The Small Server can accommodate your selection.";
+                    $smallServerCpu -= $selectedCpu;
+                    $smallServerRam -= $selectedRam;
+                    $smallServerSsd -= $selectedSsd;
+                    break;
+                case ($selectedCpu <= $mediumServerCpu && $selectedRam <= $mediumServerRam && $selectedSsd <= $mediumServerSsd):
+                    echo "The Medium Server can accommodate your selection.";
+                    $mediumServerCpu -= $selectedCpu;
+                    $mediumServerRam -= $selectedRam;
+                    $mediumServerSsd -= $selectedSsd;
+                    break;
+                case ($selectedCpu <= $bigServerCpu && $selectedRam <= $bigServerRam && $selectedSsd <= $bigServerSsd):
+                    echo "The Big Server can accommodate your selection.";
+                    $bigServerCpu -= $selectedCpu;
+                    $bigServerRam -= $selectedRam;
+                    $bigServerSsd -= $selectedSsd;
+                    break;
+                default:
+                    echo "No server can accommodate your selection.";
+                    break;
+            }
+            
+            echo $smallServerCpu;
+            echo $smallServerRam;
+            echo $smallServerSsd;
         
             $totalPrice = $selectedCpu + $selectedRam + $selectedSsd;
-            
-            
+
         }
-        $SmallServer = [
-          "Cores" => 4,
-          "Ram"   => 32768,
-          "SSD"   => 4000,
-        ];
 
-        $MediumServer = [
-          "Cores" => 8,
-          "Ram"   => 65536,
-          "SSD"   => 8000,
-        ];
+        $daten = file_get_contents("serverleistung.txt");
+        $alles = explode(";",$daten);
+        $server = $alles[0];
 
-        $BigServer = [
-          "Cores" => 16,
-          "Ram"   => 131072,
-          "SSD"   => 16000,
-        ];
         
-        $ArraySmall = implode(";",$SmallServer);
-        $ArrayMedium = implode(";",$MediumServer);
-        $ArrayBig = implode(";",$BigServer);
-        $file="serverleistung.txt";
-        $AllgemeinServerleistung = "Small".";". $ArraySmall . "\n" ."Medium". ";" . $ArrayMedium . "\n" . "Big". ";" . $ArrayBig . "\n";
-        file_put_contents($file,$AllgemeinServerleistung);
-
+        
+         
 
         ?>
 
-<form action="<?php echo $_SERVER['PHP_SELF']; ?>" method="post">        
+<form action="<?php echo $_SERVER['PHP_SELF']; ?>" method="post">  
+        <label for="KundeName">Geben Sie Ihren Namen ein</label>
+        <input type="text" name="KundeName" id="Kundename">
         <label for="cpu">CPU (Cores): </label>
         <select id="cpu" name="cpu">
-            <option value="5">1 Core - 5 CHF</option>
-            <option value="10">2 Cores - 10 CHF</option>
-            <option value="18">4 Cores - 18 CHF</option>
-            <option value="30">8 Cores - 30 CHF</option>
-            <option value="45">16 Cores - 45 CHF</option>
+            <option value="1">1 Core - 5 CHF</option>
+            <option value="2">2 Cores - 10 CHF</option>
+            <option value="4">4 Cores - 18 CHF</option>
+            <option value="8">8 Cores - 30 CHF</option>
+            <option value="16">16 Cores - 45 CHF</option>
         </select>
         <label for="ram">RAM (MB):</label>
         <select id="ram" name="ram">
-            <option value="5">512 MB - 5 CHF</option>
-            <option value="10">1024 MB - 10 CHF</option>
-            <option value="20">2048 MB - 20 CHF</option>
-            <option value="40">4096 MB - 40 CHF</option>
-            <option value="80">8192 MB - 80 CHF</option>
-            <option value="160">16384 MB - 160 CHF</option>
-            <option value="320">32768 MB - 320 CHF</option>
+            <option value="512">512 MB - 5 CHF</option>
+            <option value="1024">1024 MB - 10 CHF</option>
+            <option value="2048">2048 MB - 20 CHF</option>
+            <option value="4096">4096 MB - 40 CHF</option>
+            <option value="8192">8192 MB - 80 CHF</option>
+            <option value="16384">16384 MB - 160 CHF</option>
+            <option value="32768">32768 MB - 320 CHF</option>
         </select>
         <label for="ssd">SSD (GB):</label>
         <select id="ssd" name="ssd">
-            <option value="5">10 GB - 5 CHF</option>
-            <option value="10">20 GB - 10 CHF</option>
-            <option value="20">40 GB - 20 CHF</option>
-            <option value="40">80 GB - 40 CHF</option>
-            <option value="120">240 GB - 120 CHF</option>
-            <option value="250">500 GB - 250 CHF</option>
-            <option value="500">1000 GB - 500 CHF</option>
+            <option value="10">10 GB - 5 CHF</option>
+            <option value="20">20 GB - 10 CHF</option>
+            <option value="40">40 GB - 20 CHF</option>
+            <option value="80">80 GB - 40 CHF</option>
+            <option value="240">240 GB - 120 CHF</option>
+            <option value="500">500 GB - 250 CHF</option>
+            <option value="1000">1000 GB - 500 CHF</option>
         </select>
         
         <input type="submit" value="Bestellen">
     </form> 
-    <div> 
+    <!--
+   <div> 
         <?php echo $totalPrice ?>
     </div>
-
+      -->
     
 </body>
 </html>
