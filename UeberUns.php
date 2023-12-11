590d673eeb5b2662cdde48feda66fe2f5ad5fdbe
Onedrican <v.bernardini@hispeed.ch>
2023-12-11 13:37:16 +0100

    idfk

--- UeberUns.php
+++ UeberUns.php
@@ -7,26 +7,6 @@
     <link rel="stylesheet" href="style.css">
 </head>
 <body>
-    <nav>
-        <div class="navbar">
-          <div class="container nav-container">
-              <input class="checkbox" type="checkbox" name="" id="" />
-              <div class="hamburger-lines">
-                <span class="line line1"></span>
-                <span class="line line2"></span>
-                <span class="line line3"></span>
-              </div>  
-            <div class="logo">
-              <h1>Omni Cloud</h1>
-            </div>
-            <div class="menu-items">
-              <li><a href="index.php">Home</a></li>
-              <li><a href="Angebot.php">Angebot</a></li>
-              <li><a href="UeberUns.php">Über uns</a></li>
-              <li><a href="Kontakt.php">Kontakt</a></li>
-            </div>
-          </div>
-        </div>
-      </nav>
+
 </body>
 </html>
