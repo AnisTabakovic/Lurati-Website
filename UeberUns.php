b73907882014c27bf6c23f6da08b818e279f3b18
Anis <95578459+AnisTabakovic@users.noreply.github.com>
2023-12-15 21:37:55 +0100

    Update UeberUns.php

--- UeberUns.php
+++ UeberUns.php
@@ -15,19 +15,19 @@
 </ul> 
 
 <div class="about-section">
-  <h1>  Überber uns</h1>
+  <h1>  Über uns</h1>
   <p>Wir sind 3 Schüler*innen der I2a, welche den Auftrag erhalten haben eine Website aufzusetellen die VMs anbietet.</p>
-  <p>Resize the browser window to see that this page is responsive by the way.</p>
+  <p>Falls ihr txt habt rein damit.</p>
 </div>
 
 <h2 style="text-align:center">Our Team</h2>
 <div class="row">
   <div class="column">
     <div class="card">
-      <img src="/w3images/team1.jpg" alt="Jane" style="width:100%">
+      <img src="/w3images/team1.jpg" alt="Anis" style="width:100%">
       <div class="container">
         <h2>Jane Doe</h2>
-        <p class="title">CEO & Founder</p>
+        <p class="title">CEO</p>
         <p>Some text that describes me lorem ipsum ipsum lorem.</p>
         <p>jane@example.com</p>
         <p><button class="button">Contact</button></p>
