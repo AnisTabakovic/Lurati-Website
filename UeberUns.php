49a3f446e368b2516b5fb76a34ff41aabbe34bd0
Anis <95578459+AnisTabakovic@users.noreply.github.com>
2023-12-15 21:49:13 +0100

    Update UeberUns.php

--- UeberUns.php
+++ UeberUns.php
@@ -24,9 +24,9 @@
 <div class="row">
   <div class="column">
     <div class="card">
-      <img src="/w3images/team1.jpg" alt="Jane" style="width:100%">
+      <img src="/w3images/team1.jpg" alt="Anis" style="width:100%">
       <div class="container">
-        <h2>Jane Doe</h2>
+        <h2>Anis Tabakovic</h2>
         <p class="title">CEO & Founder</p>
         <p>Some text that describes me lorem ipsum ipsum lorem.</p>
         <p>jane@example.com</p>
@@ -37,9 +37,9 @@
 
   <div class="column">
     <div class="card">
-      <img src="/w3images/team2.jpg" alt="Mike" style="width:100%">
+      <img src="/w3images/team2.jpg" alt="Vincent" style="width:100%">
       <div class="container">
-        <h2>Mike Ross</h2>
+        <h2>Vincent Bernardini</h2>
         <p class="title">Art Director</p>
         <p>Some text that describes me lorem ipsum ipsum lorem.</p>
         <p>mike@example.com</p>
@@ -50,9 +50,9 @@
 
   <div class="column">
     <div class="card">
-      <img src="/w3images/team3.jpg" alt="John" style="width:100%">
+      <img src="/w3images/team3.jpg" alt="Mirella" style="width:100%">
       <div class="container">
-        <h2>John Doe</h2>
+        <h2>Mirella Damiano</h2>
         <p class="title">Designer</p>
         <p>Some text that describes me lorem ipsum ipsum lorem.</p>
         <p>john@example.com</p>
